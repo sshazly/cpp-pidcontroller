373accc14e0b3ea3307e568ddc5cc43e6a94595b
Sherif Shazly <sshazly@ncsu.edu>
2017-09-26 22:05:18 -0500

    Corrected formatting and updated readme file

--- app/main.cpp
+++ app/main.cpp
@@ -10,23 +10,29 @@
 #include <PID.hpp>
 
 int main() {
-  double kp = 0;
-  double ki = 0;
-  double kd = 0;
-  double target = 0;
-  double val = 0;
-  double dt = 0.1;
-  cout << "Please input Kp, Ki, Kd for PID control (seperated by a space)" << endl;
-  cin >> kp >> ki >> kd;
-  cout << "Please input setpoint, initial velocity, delta time (seperated by a space)" << endl;
-  cin >> target >> val >> dt;
-  class PID pid; // Declare pid as class PID
-  pid.setGain(kp,ki,kd); // Set gain values (pointless, not used in calculation)
-  pid.initialize(dt);
-  for (int i = 0; i < 100; i++) {
-        double inc = pid.calculate(target, val); // Calculate increment
-        printf("velocity:% 7.3f     increment:% 7.3f\n", val, inc); // Print values to console
-        val += inc; // add increment to dynamic value
-  }
-  return 0;
+	double kp = 0;
+	double ki = 0;
+	double kd = 0;
+	double target = 0;
+	double val = 0;
+	double dt = 0.1;
+	cout << "Please input Kp, Ki, Kd for PID control (seperated by a space)"
+			<< endl;
+	cin >> kp >> ki >> kd;
+	cout
+			<< "Please input setpoint, initial velocity, delta time (seperated by a space)"
+			<< endl;
+	cin >> target >> val >> dt;
+	class PID pid;           /// Declare pid as class PID
+	pid.setGain(kp, ki, kd); /// Set gain values
+	pid.initialize(dt);
+	for (int i = 0; i < 100; i++) {
+				 /// Calculate increment
+		double inc = pid.calculate(target, val); 
+				 /// Print values to console
+		printf("velocity:% 7.3f     increment:% 7.3f\n", val, inc); 
+		val += inc; 	 /// add increment to dynamic variable val
+	}
+
+	return 0;
 }
